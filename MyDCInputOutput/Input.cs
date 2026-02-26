3eef9408a7a7f47a38136756d335b6f8086fd86b
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-02-26 12:39:22 +0500

    Add project files.

---
+++ MyDCInputOutput/Input.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyDCInputOutput
+{
+    public class Input
+    {
+        /// <summary>
+        /// Читает одну строку пользовательского ввода
+        /// </summary>
+        /// <returns></returns>
+        internal static string? Data()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            var choice = Console.ReadLine();
+            Console.ResetColor();
+            return choice;
+        }
+
+        /// <summary>
+        /// Красиво печатает меню и возвращает выбор
+        /// </summary>
+        /// <param name="menu">Печатаемое меню</param>
+        /// <param name="message">Заглавие к меню</param>
+        /// <returns>Номер выбранного пункта меню</returns>
+        internal static uint MenuAction(string[] menu, string message)
+        {
+            uint action;
+            string? choice;
+            do
+            {
+                bool isCorrectAction;
+                do
+                {
+                    Output.Message(message, ConsoleColor.DarkYellow);
+                    for (int i = 0; i < menu.Length; i++)
+                    {
+                        Output.Message($" {i + 1}.  {menu[i]}\n\n", ConsoleColor.White);
+                    }
+
+                    Output.Message("Введите номер выбранного действия:  ", ConsoleColor.White);
+                    isCorrectAction = uint.TryParse(Input.Data(), out action);
+
+                    if (action > menu.Length || action == 0)
+                    {
+                        Output.Error("Нераспознанная команда! Проверьте корректность ввода");
+                        isCorrectAction = false;
+                    }
+                } while (!isCorrectAction);
+
+                Output.Message($"Вы выбрали дейстиве: {menu[action - 1]}\n", ConsoleColor.White);
+                Output.Message("Вы уверены в своём выборе? Если уверены, напишите ДА(в любом регистре), любой другой ввод будет воспринят как НЕТ:  ", ConsoleColor.White);
+                choice = Input.Data();
+
+            } while (!string.Equals(choice, "Да", StringComparison.OrdinalIgnoreCase));
+            return action;
+        }
+
+        /// <summary>
+        /// Читает целое число в пределах типа int
+        /// </summary>
+        /// <param name="message">Приглашенние к вводу числа</param>
+        /// <param name="error">Сообщение об ошибке</param>
+        /// <returns></returns>
+        internal static int IntNumber(string message, string error)
+        {
+            bool isCorrect;
+            int number;
+            do
+            {
+                Output.Message(message, ConsoleColor.White);
+                isCorrect = int.TryParse(Data(), out number);
+
+                if (!isCorrect)
+                    Output.Error(error);
+
+            } while (!isCorrect);
+            return number;
+        }
+
+    }
+}
---
+++ MyDCInputOutput/Output.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyDCInputOutput
+{
+    internal class Output
+    {
+        /// <summary>
+        /// Сообщает об ошибках
+        /// </summary>
+        /// <param name="error">Печатаемая ошибка</param>
+        internal static void Error(string error) => Message($"Ошибка: {error}!\n", ConsoleColor.Red);
+
+        /// <summary>
+        /// Печатаем красивые сообщения пользователю
+        /// </summary>
+        /// <param name="message">Сообщение на печать</param>
+        /// <param name="color">Цвет печать</param>
+        public static void Message(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Печатает строки
+        /// </summary>
+        /// <param name="color">Цвет печати</param>
+        /// <param name="messages">Массив строк</param>
+        public static void Message(ConsoleColor color, params string[] messages)
+        {
+            Console.ForegroundColor = color;
+            foreach (var message in messages)
+            {
+                Console.WriteLine(message);
+            }
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Печатает разделители
+        /// </summary>
+        public static void Separator() => Message("<===><=====><========><============>\n\n", ConsoleColor.DarkGreen);
+    }
+}
